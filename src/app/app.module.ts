250d6099effe4e61b9ec1c1a5f146f971f416ef3
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-28 23:28:08 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,31 @@ import { AppComponent } from './app.component';
 import { AddNewsComponent } from './add-news/add-news.component';
 import { FormsModule } from '@angular/forms';
 import { ViewNewsComponent } from './view-news/view-news.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoter:Routes=[
+  {
+    path:"",
+    component:AddNewsComponent
+  },
+  {
+    path:"view",
+    component:ViewNewsComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddNewsComponent,
-    ViewNewsComponent
+    ViewNewsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(myRoter),
     FormsModule
   ],
   providers: [],
