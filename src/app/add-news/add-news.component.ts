a760dfdc10a139e6512c1a44956ee56bcc51ed15
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-28 22:37:23 +0530

    add

--- src/app/add-news/add-news.component.ts
+++ src/app/add-news/add-news.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-news.component.css']
 })
 export class AddNewsComponent {
+  title=""
+  author=""
+  description=""
+  image=""
+  publishingDate=""
+  content=""
+
+  readValues=()=>
+  {
+    let data:any={"title":this.title,"author":this.author,"description":this.description,"image":this.image,"publishingDate":this.publishingDate,"content":this.content}
+  console.log(data)
+  }
+
+
 
 }
